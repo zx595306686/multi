8a6eb3f67d18ac24ffefb0a9e7b4f32fa7cfc92f
zx595306686 <595306686@qq.com>
2021-06-10 16:33:27 +0800

    multi
    多车主机

---
+++ multi_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/autolabor/multi_ws/src
+# Build directory: /home/autolabor/multi_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("multi_driver")
+subdirs("ira_laser_tools")
+subdirs("multi_navigation")
+subdirs("multi_description")
